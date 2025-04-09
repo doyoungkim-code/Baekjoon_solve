5d9e1f788f5ab2f2f5090fcd01157cc23ad7ee4a
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-04-10 03:50:42 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <deque>
+#include <algorithm>
 
 int main(void){
     int n, num, next;
-    std::deque <int> dq;
-    std::deque <int> dq_index;
+    std::deque <std::pair <int, int>> dq;
     std::cin >> n;
     for (int i = 0; i < n; i ++){
         std::cin >> num;
-        dq.push_back(num);
-        dq_index.push_back(i + 1);
+        dq.push_back({num, i + 1});
     }
     for (int i = 0; i < n; i ++){
-        next = dq.front();
+        next = dq.front().first;
+        std::cout << dq.front().second << " ";
         dq.pop_front();
-        std::cout << dq_index.front() << " ";
-        dq_index.pop_front();
         if (next > 0){
             for (int j = 0; j < next - 1; j ++){
                 dq.push_back(dq.front());
                 dq.pop_front();
-                dq_index.push_back(dq_index.front());
-                dq_index.pop_front();
             }
         }
         else {
             for (int j = 0; j < - next; j ++){
                 dq.push_front(dq.back());
                 dq.pop_back();
-                dq_index.push_front(dq_index.back());
-                dq_index.pop_back();
             }
         }
     }
