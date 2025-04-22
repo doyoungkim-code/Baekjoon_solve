3ff64c2a08a4e9f3e278e0f0d08f39dcefafd70f
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-04-22 23:25:53 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void){
+    int T, a, b;
+    std::cin >> T;
+    for (int i = 0; i < T; i ++)
+    {
+        std::cin >> a >> b;
+        std::cout << a + b << "\n";
+    }
+}
