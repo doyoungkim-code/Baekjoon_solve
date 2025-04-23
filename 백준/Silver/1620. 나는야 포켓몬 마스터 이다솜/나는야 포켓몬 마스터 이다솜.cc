c322937c417652c8c5163bbc9926385c8bd49df6
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-04-23 21:17:39 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 136 ms, Memory: 20732 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -4,24 +4,23 @@
 
 int main(void){
     int N, M;
-    std::string name;
+    std::string input;
+    std::string name[100001];
     std::map <std::string, int> m;
-    std::map <int, std::string> m_rev;
     std::cin.tie(NULL);
     std::ios::sync_with_stdio(false);
     std::cin >> N >> M;
     for (int i = 1; i <= N; i ++)
     {
-        std::cin >> name;
-        m.insert({name, i});
-        m_rev.insert({i, name});
+        std::cin >> name[i];
+        m.insert({name[i], i});
     }
     for (int i = 0; i < M; i ++)
     {
-        std::cin >> name;
-        if (name[0] >= '0' && name[0] <= '9')
-            std::cout << m_rev.find(stoi(name)) -> second << "\n";
-        else if ((name[0] >= 'A' && name[0] <= 'Z') || (name[0] >= 'a' && name[0] <= 'z'))
-            std::cout << m.find(name) -> second << "\n";
+        std::cin >> input;
+        if (input[0] >= '0' && input[0] <= '9')
+            std::cout << name[stoi(input)] << "\n";
+        else if ((input[0] >= 'A' && input[0] <= 'Z') || (input[0] >= 'a' && input[0] <= 'z'))
+            std::cout << m.find(input) -> second << "\n";
     }
 }
