be2f7d0afdffd3f4268f8a0a382ed85f114b8a41
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-05-23 17:50:42 +0900

    [Silver II] Title: 부분수열의 합, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,47 @@
+// 1182
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <int> v;
+vector <bool> visited;
+int result = 0;
+
+void backtrack(int index, int N, int S, int sum, int depth)
+{
+	if (sum == S && depth > 0)
+	{
+		result++;
+	}
+	for (int i = index; i <= N; ++i)
+	{
+		if (!visited[i])
+		{
+			sum = sum + v[i];
+			visited[i] = true;
+			backtrack(i + 1, N, S, sum, depth + 1);
+			sum = sum - v[i];
+			visited[i] = false;
+		}
+	}
+}
+
+int main(void)
+{
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	
+	int N, S;
+	cin >> N >> S;
+	v = vector <int> (N + 1, 0);
+	visited = vector <bool> (N + 1, false);
+	for (int i = 1; i <= N; ++i)
+	{
+		cin >> v[i];
+	}
+	backtrack(1, N, S, 0, 0);
+	cout << result;
+}
