7fc1acacd5022706e5535984a873b37d5252c7a9
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-07-28 23:31:29 +0900

    [Silver IV] Title: 스택 2, Time: 884 ms, Memory: 238940 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,56 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		Stack <Integer> stack = new Stack<>();
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		int n = sc.nextInt();
-		
-		for (int i = 0; i < n; i++)
-		{
-			int order = sc.nextInt();
-			if (order == 1)
-			{
-				stack.push(sc.nextInt());
+		Stack<Integer> stack = new Stack<>();
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int order = Integer.parseInt(st.nextToken());
+
+			if (order == 1) {
+				int x = Integer.parseInt(st.nextToken());
+				stack.push(x);
 			}
-			else if (order == 2)
-			{
-				if (!stack.isEmpty())
-				{
+			else if (order == 2) {
+				if (!stack.isEmpty()) {
 					sb.append(stack.pop()).append("\n");
-				}
-				else
-				{
+				} else {
 					sb.append(-1).append("\n");
 				}
-					
 			}
-			else if (order == 3)
-			{
+			else if (order == 3) {
 				sb.append(stack.size()).append("\n");
 			}
-			else if (order == 4)
-			{
+			else if (order == 4) {
 				sb.append(stack.isEmpty() ? 1 : 0).append("\n");
 			}
-			else if (order == 5)
-			{
-				if (!stack.isEmpty())
-				{
+			else if (order == 5) {
+				if (!stack.isEmpty()) {
 					sb.append(stack.peek()).append("\n");
-				}
-				else
-				{
+				} else {
 					sb.append(-1).append("\n");
 				}
 			}
 		}
-		System.out.print(sb);
-		sc.close();
+		System.out.print(sb.toString());
 	}
 }
-
