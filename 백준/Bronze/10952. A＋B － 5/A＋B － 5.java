7cf9c84f125891f0bdd1fb4dd5aa1d221f042cdc
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-08-02 23:19:24 +0900

    [Bronze V] Title: A+B - 5, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String line = br.readLine();
+		while (line != null) {
+			String[] str = line.split(" ");
+			int A = Integer.parseInt(str[0]);
+			int B = Integer.parseInt(str[1]);
+			if (A == 0 && B == 0)
+				break;
+			System.out.println(A + B);
+			line = br.readLine();
+		}
+		
+	}
+}
