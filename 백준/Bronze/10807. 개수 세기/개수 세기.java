507f1810157e47ab6b5e42ef799c38b321e1298b
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-08-06 01:47:48 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] str = br.readLine().split(" ");
+		int v = Integer.parseInt(br.readLine());
+		
+		int sum = 0;
+		for (int i = 0; i < N; ++i) {
+			if (Integer.parseInt(str[i]) == v)
+			{
+				sum ++;
+			}
+		}
+		System.out.println(sum);
+	}
+}
