ec347658f70b669d59a549ec925874d4991c48a1
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-08-14 17:50:10 +0900

    [Silver II] Title: N과 M (12), Time: 292 ms, Memory: 23384 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static int[] arr;
+	static int[] result;
+	static StringBuilder sb = new StringBuilder();
+	
+	private static void dfs(int depth)
+	{
+		if (depth == M)
+		{
+			boolean flag = false;
+			for (int i = 0; i < M - 1; ++i)
+			{
+				if (result[i] <= result[i + 1])
+					continue;
+				else
+					flag = true;
+			}
+			if (!flag)
+			{
+				for (int i = 0; i < M; ++i)
+				{
+					sb.append(result[i] + " ");
+				}
+				sb.append("\n");
+			}
+			return ;
+		}
+		
+		int temp = -1;
+		for (int i = 0; i < N; ++i)
+		{
+			if (arr[i] != temp)
+			{
+				temp = arr[i];
+				result[depth] = arr[i];
+				dfs(depth + 1);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		result = new int[M];
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; ++i)
+		{
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		dfs(0);
+		
+		System.out.println(sb);
+	}
+
+}
