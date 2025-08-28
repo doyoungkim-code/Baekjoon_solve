576d05b6acd083132749ce379b32afbdf38037e9
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-08-28 20:30:02 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 726 ms, Memory: 81,276 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,119 +1,90 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
-	static Edge[] edgeList;
-	static int[] parents;
-	
-	static class Edge implements Comparable<Edge>
-	{
-		
-		int from;
-		int to;
-		long weight;
-		
-		public Edge(int from, int to, long weight) {
-			super();
-			
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
-		}
-		
-		@Override
-		public int compareTo(Edge o) {
-			return (Long.compare(this.weight, o.weight));
-		}
-	}
-	
-	static int find(int a)
-	{
-		if (parents[a] == a)
-			return a;
-		return (parents[a] = find(parents[a]));
-	}
-	
-	static boolean union(int a, int b)
-	{
-		int rootA = find(a);
-		int rootB = find(b);
-		
-		if (rootA == rootB)
-			return false;
-		
-		if (rootA > rootB)
-			parents[rootB] = rootA;
-		else
-			parents[rootA] = rootB;
-		return true;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc <= T; ++tc)
-		{
-			// 파싱부
-			int N = Integer.parseInt(br.readLine());
-			edgeList = new Edge[N * (N - 1) / 2];
-			parents = new int[N + 1];
-			long[] Nx = new long[N];
-			long[] Ny = new long[N];
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; ++i)
-				Nx[i] = Long.parseLong(st.nextToken());
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; ++i)
-				Ny[i] = Long.parseLong(st.nextToken());
-			double E = Double.parseDouble(br.readLine());
-			
-			// 실행부
-			for (int i = 0; i <= N; ++i)
-			{
-				parents[i] = i;
-			}
-			
-			int numEdge = 0;
-			
-			for (int i = 0; i < N - 1; ++i)
-			{
-				for (int j = i + 1; j < N; ++j)
-				{
-					long dx = Nx[i] - Nx[j];
-					long dy = Ny[i] - Ny[j];
+    
+    static List<Edge> edgeList;
+    
+    static class Edge implements Comparable<Edge>{
+        int to;
+        long range;
+        
+        public Edge(int to, long range) {
+            this.to = to;
+            this.range = range;
+        }
 
-					long len = dx * dx + dy * dy;
-					edgeList[numEdge] = new Edge(i, j, len);
-					numEdge ++;
-				}
-			}
-			
-			Arrays.sort(edgeList);
-			
-			long res = 0;
-			int cnt = 0;
-			
-			for (Edge e : edgeList)
-			{
-				if (!union(e.from, e.to))
-					continue;
-				res += e.weight;
-				if (++cnt == N - 1)
-					break;
-			}
-			
-			long finalres = Math.round(E * res);
-			
-			sb.append("#" + tc + " " + finalres + "\n");
-		}
-		System.out.println(sb);
-	}
+        @Override
+        public int compareTo(Edge o) {
+            return (Long.compare(this.range, o.range));
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int tc = 1; tc <= T; ++tc)
+        {
+            int N = Integer.parseInt(br.readLine());    
+            long[] Nx = new long[N];
+            long[] Ny = new long[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; ++i)
+                Nx[i] = Long.parseLong(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; ++i)
+                Ny[i] = Long.parseLong(st.nextToken());
+            double E = Double.parseDouble(br.readLine());
+            
+            edgeList = new ArrayList<>();
+            
+            long totalRange = 0;
+            int count = 0;
+            
+            boolean[] visited = new boolean[N];
+            PriorityQueue<Edge> pq = new PriorityQueue<>();
+            pq.offer(new Edge(0, 0));
+            
+            while (!pq.isEmpty())
+            {
+                Edge pollEdge = pq.poll();
+                int pollEdgeTo = pollEdge.to;
+                long pollEdgeRange = pollEdge.range;
+                
+                if (visited[pollEdgeTo])
+                	continue;
+                
+                visited[pollEdgeTo] = true;
+                totalRange += pollEdgeRange;
+                count ++;
+                
+                for (int i = 0; i < N; ++i)
+                {
+                	if (!visited[i])
+                	{
+                		long dx = Nx[pollEdgeTo] - Nx[i];
+                		long dy = Ny[pollEdgeTo] - Ny[i];
+                		
+                		long dist = dx * dx + dy * dy;
+                		
+                		pq.add(new Edge(i, dist));
+                	}
+                }
+            }
+            
+            long finalRange = Math.round(totalRange * E);
+            sb.append("#" + tc + " " + finalRange + "\n");
+        }
+        System.out.println(sb);
+        
+    }
 }
