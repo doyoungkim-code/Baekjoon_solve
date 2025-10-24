269f3a5cae7bc4c1a3393f64e954b79d769bc00f
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-10-24 15:59:11 +0900

    [Silver I] Title: 절댓값 힙, Time: 524 ms, Memory: 25228 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+class Main {
+	
+	static class Num implements Comparable<Num>{
+		int val;
+		
+		public Num(int val) {
+			super();
+			this.val = val;
+		}
+		@Override
+		public int compareTo(Num o) {
+			// TODO Auto-generated method stub
+			if (Integer.compare(Math.abs(this.val), Math.abs(o.val)) == 0)
+			{
+				if (this.val < o.val)
+					return (-1);
+				else
+					return (1);
+			}
+			return (Integer.compare(Math.abs(this.val), Math.abs(o.val)));
+		}
+		
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Num> pq = new PriorityQueue<>();
+		
+		for (int i = 0; i < N; ++i)
+		{
+			int input = Integer.parseInt(br.readLine());
+			
+			if (input != 0)
+			{
+				pq.add(new Num(input));
+			}
+			else
+			{
+				if (pq.isEmpty())
+					System.out.println("0");
+				else
+					System.out.println(pq.poll().val);
+			}
+		}
+	}
+}
