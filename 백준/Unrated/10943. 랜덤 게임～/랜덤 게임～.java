59fc9038549c7476446f09a0c4cfa47a642782c8
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-11-04 14:44:10 +0900

    [Unrated] Title: 랜덤 게임~, Time: 60 ms, Memory: 11416 KB -BaekjoonHub

---
+++ 백준/Unrated/10943. 랜덤 게임～/랜덤 게임～.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+		
+		System.out.println(random.nextInt(10) + 1);
+	}
+
+}
