88edc0fada641118a73e1d042a0df91e6de25e28
doyoung <138653105+doyoungkim-code@users.noreply.github.com>
2025-11-28 11:15:35 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		
+		System.out.println((A * B) / 2);
+	}
+
+}
